4477dbe2233ce28e8fcdf50337738d7fe1404c94
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-10-06 15:49:43 +0300

    UIImage'leri CIImage'e çevirdim şimdi fonksiyonları yazmaya devam edicem.

--- MachineLearningImageTest/ViewController.swift
+++ MachineLearningImageTest/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    var chosenImage = CIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+        
+        if let ciImage = CIImage(image: imageView.image!) {
+            chosenImage = ciImage
+        }
+        recognizeImage(image: chosenImage )
+    }
+    
+    
+    func recognizeImage(image : CIImage) {
+        
+        
+    }
+    
 }
 
